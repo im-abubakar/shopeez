6d16063d89dfefd42c2e3a7ee773eee932a74f0f
bakkarrr <bakarbro123@gmail.com>
2024-07-13 16:32:34 +0500

    store functionality completed

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export function Footer() {
+    return (
+        <footer className='bg-grey text-white'>
+            <div className='max-w-[1240px] mx-auto py-2 text-sm sm:text-base'>
+                <div className='flex justify-center flex-col items-center'>
+                    <p className='text-center'>© Copyright 2023 Shopeez | All Rights Reserved</p>
+ 
+                </div>
+            </div>
+        </footer>
+    )
+}
